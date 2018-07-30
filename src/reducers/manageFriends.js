d3bb22f8fa85d9648f25821e22685b341e20dcf8
Randall Robinson <keith_adams007@yahoo.com>
2018-07-30 18:26:15 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,7 +2,7 @@ export function manageFriends(state, action){
   switch (action.type) {
     case 'ADD_FRIEND':
     let newfriends = state.friends.push(action.friend)
-      return {friends: action.friend}
+      return {friends: newfriends}
     case 'REMOVE_FRIEND':
     return {}
     default:
